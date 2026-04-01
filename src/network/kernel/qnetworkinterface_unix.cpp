15750561c9c0266ae2af2de0694d0e1f56028845
Christian Prochaska <christian.prochaska@genode-labs.com>
2026-04-01 06:01:40 +0200

    qtbase_freebsd_crossbuild_genode_libc_headers.patch

--- src/network/kernel/qnetworkinterface_unix.cpp
+++ src/network/kernel/qnetworkinterface_unix.cpp
@@ -382,7 +382,7 @@ static void getAddressExtraInfo(QNetworkAddressEntry *entry, struct sockaddr *sa
     Q_UNUSED(ifname);
 }
 
-# elif defined(Q_OS_BSD4)
+# elif defined(Q_OS_BSD4) && 0
 QT_BEGIN_INCLUDE_NAMESPACE
 #  include <net/if_dl.h>
 #if defined(QT_PLATFORM_UIKIT)
