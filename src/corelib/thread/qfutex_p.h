91dc2025805698f6550c018359bdf1882f827615
Christian Prochaska <christian.prochaska@genode-labs.com>
2026-05-08 15:20:40 +0200

    qtbase_freebsd_crossbuild_genode_libc_headers.patch

--- src/corelib/thread/qfutex_p.h
+++ src/corelib/thread/qfutex_p.h
@@ -35,7 +35,7 @@ QT_END_NAMESPACE
 
 #if defined(Q_OS_DARWIN)
 #  include "qfutex_mac_p.h"
-#elif defined(Q_OS_FREEBSD)
+#elif defined(Q_OS_FREEBSD) && 0
 #  include "qfutex_freebsd_p.h"
 #elif defined(Q_OS_LINUX) && !defined(QT_LINUXBASE)
 // use Linux mutexes everywhere except for LSB builds
